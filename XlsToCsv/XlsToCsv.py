d3cc7cb4ba47823115383a8160d0d04d92739ada
FortTell <efremoff-igor@mail.ru>
2018-10-01 12:23:00 +0500

    parsing complete

--- XlsToCsv/XlsToCsv.py
+++ XlsToCsv/XlsToCsv.py
@@ -3,8 +3,12 @@
 import xlrd
 import io
 
-def is_article(s):
-    return isinstance(s, float) and s == int(s)
+def parse_cell(s, i):
+
+    if not isinstance(s, float):
+        return str.replace(str(s), ';', ',')
+    precision = 0 if i == 0 else 2
+    return ("{0:."+str(precision)+"f}").format(s)
 
 rb = xlrd.open_workbook('1.xlsm')
 csv = []
@@ -12,7 +16,7 @@ for i in range(rb.nsheets):
     sheet = rb.sheet_by_index(i)
     for j in range(sheet.nrows):
         row = sheet.row_values(j)
-        row_csv = [str.replace(str(cell), ';', ',') for cell in row]        
+        row_csv = [parse_cell(row[k], k) for k in range(len(row))]   
         csv.append(';'.join(row_csv) + '\n')
 
 with io.open('out.csv', 'w', encoding='utf8') as f:
