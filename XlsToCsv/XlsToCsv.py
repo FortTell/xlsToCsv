8e9f345a50d019492d40f07cb238b05aa3a40dd2
FortTell <efremoff-igor@mail.ru>
2018-10-01 10:20:55 +0500

    Initial commit

---
+++ XlsToCsv/XlsToCsv.py
@@ -0,0 +1,17 @@
+import xlrd
+import io
+
+rb = xlrd.open_workbook('1.xlsm')
+csv = []
+for i in range(rb.nsheets):
+    sheet = rb.sheet_by_index(i)
+    for j in range(sheet.nrows):
+        row = sheet.row_values(j)
+        row_csv = []
+        for cell in row:
+            row_csv.append(str(cell))
+        csv.append(';'.join(row_csv) + '\n')
+
+with io.open('out.csv', 'w') as f:
+    f.writelines(csv)
+
