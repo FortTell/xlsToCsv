e7e706c04d6cc7b084716e442254bed77ca4d3f4
FortTell <efremoff-igor@mail.ru>
2018-10-01 12:03:41 +0500

    simplified creation of csv line and replaced ; in cells

--- XlsToCsv/XlsToCsv.py
+++ XlsToCsv/XlsToCsv.py
@@ -3,17 +3,17 @@
 import xlrd
 import io
 
+def is_article(s):
+    return isinstance(s, float) and s == int(s)
+
 rb = xlrd.open_workbook('1.xlsm')
 csv = []
 for i in range(rb.nsheets):
     sheet = rb.sheet_by_index(i)
     for j in range(sheet.nrows):
         row = sheet.row_values(j)
-        row_csv = []
-        for cell in row:
-            row_csv.append(str(cell))
+        row_csv = [str.replace(str(cell), ';', ',') for cell in row]        
         csv.append(';'.join(row_csv) + '\n')
 
 with io.open('out.csv', 'w', encoding='utf8') as f:
     f.writelines(csv)
-
